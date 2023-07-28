1711db93d7f592e39bcbe7b172419499876edae0
helloitsalia <salihaknsz26@gmail.com>
2023-07-27 18:44:03 -0700

    new

---
+++ Practice/03_07/script.js
@@ -0,0 +1,25 @@
+/**
+ * Practice: Building objects
+ *
+ * - Create JavaScript objects based on objects in your current environment.
+ * - Give each object an identifiable name.
+ * - Create properties to describe the objects and set their values.
+ * - Find an object that has another object inside of it to create a nested object.
+ * - Test your objects in the browser console by accessing the entire object and its specific properties.
+ */
+
+var person = {
+    "name": "John Doe",
+    "age": 30,
+    "gender": "male",
+    "address": {
+      "street": "123 Main Street",
+      "city": "Anytown",
+      "state": "CA",
+      "zip code": "12345"
+    }
+};
+
+console.log("The Person object:", person);
+
+/// ilk örneğimmm ağlamak :'')))
